c6b7f2aa372ed45404ce4c076810f0f5c78a356e
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2023-01-17 15:29:22 +0100

    Network Usage

---
+++ Assets/Scripts/NetworkUsage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Alteruna;
+using UnityEngine;
+
+public class NetworkUsage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Multiplayer _multiplayer;
+    void Start()
+    {
+        _multiplayer = GetComponent<Multiplayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+         uint received = _multiplayer.NetworkStatistics.BytesPerSecondReceived;
+         uint sent = _multiplayer.NetworkStatistics.BytesPerSecondSent;
+         Debug.Log("Sent " + sent + " Received " + received);
+        }
+    }
+}

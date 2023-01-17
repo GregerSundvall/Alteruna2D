b26021b95199fe9083da85a2b1f5944f789ced6d
Daniel Turunen <89087279+DanielTurunen2021@users.noreply.github.com>
2023-01-17 14:51:22 +0100

    Started working on a powerup
    Started working on a powrup system, added functions for spawning and running.

---
+++ Assets/Powerups.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Alteruna;
+using UnityEngine;
+
+
+//[SerializeField] private Powerups Powerups;
+
+
+
+public class Powerups : Synchronizable
+{
+    private Spawner _PowerupSpawner;
+    [SerializeField] private Powerups _powerups;
+
+    private void Run()
+    {
+        _PowerupSpawner = GetComponent<Spawner>();
+        _PowerupSpawner.SpawnableObjects.Add(_powerups.gameObject);
+    }
+
+    private void RandomizePosition()
+    {
+        Vector2 position = new Vector2(Random.Range(50, 150), Random.Range(50, 150));
+        Vector2 rotation = new Vector2(Random.Range(50, 150), Random.Range(50, 150));
+        _PowerupSpawner.Spawn(0, position, rotation);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public override void AssembleData(Writer writer, byte LOD = 100)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DisassembleData(Reader reader, byte LOD = 100)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Message ewilsandman, 𒉭 Seglarn 🎀, Greger
+
+}

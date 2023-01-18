f347653434458093e5951341c841b852ced5b518
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2023-01-18 15:21:27 +0100

    Update Fewd.cs

--- Assets/Fewd.cs
+++ Assets/Fewd.cs
@@ -43,7 +43,7 @@ public class Fewd : Synchronizable {
             rotation = transform.rotation.eulerAngles;
             
             Wrap();
-            
+            Commit();
         }
         SyncUpdate();
         

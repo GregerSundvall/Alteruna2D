3978017014fdc7f24a6bee3183c1e06b59392e79
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2023-01-19 16:48:30 +0100

    Cleanup PVP handler

--- Assets/Scripts/PvPHandler.cs
+++ Assets/Scripts/PvPHandler.cs
@@ -1,27 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Alteruna;
 using Alteruna.Trinity;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Avatar = Alteruna.Avatar;
 
 public class PvPHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Avatar _playerAvatar;
     private User _serverPlayer;
-    private Transform _playerTransform;
     private PlayerController _playerController;
     private Multiplayer _multiplayerComponent;
     //[SerializeField] private GameObject PowerupRef;
     
     void Start()
     {
-        _playerController = GetComponent<PlayerController>();    
-        _playerTransform = GetComponent<Transform>();
-        _playerAvatar = GetComponent<Avatar>();
+        _playerController = GetComponent<PlayerController>();
         _multiplayerComponent = FindObjectOfType<Multiplayer>();
         _serverPlayer = _multiplayerComponent.GetUser(0);
         //PowerupRef = GetComponent<GameObject>();
@@ -30,6 +22,7 @@ public class PvPHandler : MonoBehaviour
         {}
         _multiplayerComponent.RegisterRemoteProcedure("KillPlayer", KillProcedureFunction);
     }
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.GetComponent<Avatar>() != null)
@@ -44,37 +37,24 @@ public class PvPHandler : MonoBehaviour
                     if (otherPlayerController.Size < _playerController.Size)
                     {
                         Debug.Log("I'm bigger than you");
-                        Debug.Log("other size " + otherPlayerController.Size);
-                        Debug.Log("my size " + _playerController.Size);
                         Debug.Log(GetComponent<UniqueID>().UIDString);
-                        //User otherPlayer = other.gameObject.GetComponent<Avatar>().Possessor;
-                        //String otherGuid = other.gameObject.GetComponent<UniqueID>().UIDString;
                         other.gameObject.GetComponent<PvPHandler>().Kill();
                     }
                 }
             }
         }
     }
-    public void Kill()
+    
+    private void Kill()
     {
-       // ProcedureParameters parameters = new ProcedureParameters();
-        //parameters.Set("Guid", GetComponent<UniqueID>().UIDString);
-        _playerController.Size *= 2.0f;
-        _playerController.sizeWasUpdated = true;
-        _multiplayerComponent.InvokeRemoteProcedure("KillPlayer", UserId.AllInclusive);
+        _multiplayerComponent.InvokeRemoteProcedure("KillPlayer", UserId.All);
     }
     
-    
-
     private void KillProcedureFunction(ushort userToKill, ProcedureParameters parameters, uint callId, ITransportStreamReader processor)
     {
         Debug.Log("KILL PROCEDURE RUN");
-        //String otherGuid = parameters.Get("otherGuid", "");
-       // Debug.Log("other " + otherGuid);
-        //Debug.Log("self" + GetComponent<UniqueID>().UIDString);
-
-        transform.position = Vector3.zero;
-        _playerController.Size *= 0.1f;
+        transform.position = Vector3.zero; // placeholder position
+        _playerController.Size = 1f;
         _playerController.sizeWasUpdated = true;
         //
         // foreach (GameObject gObject in SceneManager.GetActiveScene().GetRootGameObjects()) // there must be a better way

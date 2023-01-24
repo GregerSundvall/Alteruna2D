f3a5f4ec1f2e2f6f2d3e860851ecfacfcbc27d99
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2023-01-24 13:29:06 +0100

    syncable arrays. not working. but something happens...

---
+++ Assets/Scripts/FewdData.cs
@@ -0,0 +1,8 @@
+using System;
+
+
+[Serializable] public class FewdData {
+    public float[] posX = new float[50];
+    public float[] posY = new float[50];
+    public float[] rotZ = new float[50];
+}
--- Assets/Scripts/FewdManager.cs
+++ Assets/Scripts/FewdManager.cs
@@ -1,52 +1,105 @@
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using Alteruna;
 using UnityEngine;
 
-public class FewdManager : MonoBehaviour 
+public class FewdManager : Synchronizable 
 {
-    [SerializeField] private Spawner spawner;
     private Multiplayer alterunaMP;
+    private FewdData fewdData;
     private bool iAmPlayerZero = false;
-    private List<GameObject> spawnedFewd;
+    public List<GameObject> spawnedFewd = new List<GameObject>();
+    [SerializeField] private GameObject fewdPrefab;
+    private int fewdCount = 50;
 
-    void Start()
-    {
-    }
-    
-    public void Register(GameObject go)
-    {
-        spawnedFewd.Add(go);
-    }
-    
-    public void Init()
+    private void Start()
     {
         alterunaMP = FindObjectOfType<Multiplayer>();
-        if (alterunaMP.Me == alterunaMP.GetUser(0))
+        fewdData = new FewdData();
+    }
+
+    private void Update()
+    {
+        SyncUpdate();
+    }
+
+    public void Init()
+    {
+        for (int i = 0; i < fewdCount; i++)
         {
-            iAmPlayerZero = true;
+            fewdData.posX[i] = 0;
+            fewdData.posY[i] = 0;
+            fewdData.rotZ[i] = 0;
         }
+        
+        for (int i = 0; i < fewdCount; i++)
+        {
+            var obj = Instantiate(fewdPrefab);
+            obj.GetComponent<Fewd>().fewdManager = this;
+            obj.GetComponent<Fewd>().index = i;
+            spawnedFewd.Add(obj);
+        }
+        
+        if (alterunaMP.Me == alterunaMP.GetUser(0)) { iAmPlayerZero = true; }
         
         if (iAmPlayerZero)
         {
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < fewdCount; i++)
             {
-                spawnedFewd.Add(spawner.Spawn(0));
+                fewdData.posX[i] = spawnedFewd[i].transform.position.x;
+                fewdData.posY[i] = spawnedFewd[i].transform.position.y;
+                fewdData.rotZ[i] = spawnedFewd[i].transform.rotation.eulerAngles.z;
             }
+            Commit();
         }
-    }
-    
-    public void SyncPositions()
-    {
-        foreach (var spawnedObject in spawnedFewd)
+        else
         {
-            var fewd = spawnedObject.GetComponent<Fewd>();
-            fewd.SyncPosition();
+            SyncAllFromReceivedData();
         }
     }
     
-
-    void Update()
+    public void PublishCurrentPositions()
     {
+        for (int i = 0; i < fewdCount; i++)
+        {
+            fewdData.posX[i] = spawnedFewd[i].transform.position.x;
+            fewdData.posY[i] = spawnedFewd[i].transform.position.y;
+            fewdData.rotZ[i] = spawnedFewd[i].transform.rotation.eulerAngles.z;
+        }
+        Commit();
+    }
+    
+    void SyncAllFromReceivedData()
+    {
+        if (spawnedFewd.Count != fewdCount) { return; }
         
+        for (int i = 0; i < fewdCount; i++)
+        {
+            var x = fewdData.posX[i];
+            var y = fewdData.posY[i];
+            Debug.Log(spawnedFewd.Count + " / " + i);
+            spawnedFewd[i].transform.position = new Vector3(x, y, 0);
+            spawnedFewd[i].transform.rotation = Quaternion.Euler(0, 0, fewdData.rotZ[i]);
+        }
+    }
+
+    public void PublishChangedPosition(int index)
+    {
+        fewdData.posX[index] = spawnedFewd[index].transform.position.x;
+        fewdData.posY[index] = spawnedFewd[index].transform.position.y;
+        fewdData.rotZ[index] = spawnedFewd[index].transform.rotation.eulerAngles.z;
+        Commit();
+    }
+
+    public override void AssembleData(Writer writer, byte LOD = 100)
+    {
+        writer.WriteObject(fewdData);
+    }
+
+    public override void DisassembleData(Reader reader, byte LOD = 100)
+    {
+        fewdData = reader.ReadObject() as FewdData;
+        SyncAllFromReceivedData();
     }
 }
